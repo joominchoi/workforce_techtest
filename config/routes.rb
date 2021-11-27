d462fb99dc2a39f558bb77d546e3fd591c975e8e
JooMin Choi <joominchoi23@gmail.com>
2021-11-27 13:47:23 +0000

    created users model and controller and updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "sign_up", to: "users#new"
+  get "log_in", to: "sessions#new"
+  post "log_in", to: "sessions#create"
+  delete "log_out", to: "sessions#destroy"
+  resources :users, except: [:new]
 end
