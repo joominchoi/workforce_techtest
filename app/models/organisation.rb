4641926815a84686604869699d1c2d939d6fbdd1
JooMin Choi <joominchoi23@gmail.com>
2021-11-29 16:04:35 +0000

    updated organisations model

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,3 +1,6 @@
 class Organisation < ApplicationRecord
   has_many :users
+
+  validates :name, presence: true, uniqueness: true
+  validates :hourly_rate, numericality: { greater_than_or_equal_to: 0 }
 end
