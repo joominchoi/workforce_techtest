d24f3c1552811960d8cb3877b29e438bda8848b0
JooMin Choi <joominchoi23@gmail.com>
2021-12-01 11:12:08 +0000

    modified to put test in correct controller test

--- test/controllers/shifts_controller_test.rb
+++ test/controllers/shifts_controller_test.rb
@@ -1,8 +1,7 @@
 require "test_helper"
 
 class ShiftsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get '/sign_up'
-    assert_response :success
-  end
+  # test "the truth" do
+  #   assert true
+  # end
 end
